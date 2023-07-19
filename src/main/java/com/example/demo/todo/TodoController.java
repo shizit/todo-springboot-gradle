4233f5c82f64b5b02934ed2b1d4e0c179e29a491
shizit <shizit1995@gmail.com>
2023-07-19 14:06:46 +0900

    fix: Enter押下時に想定外のpostがされるバグを修正

--- src/main/java/com/example/demo/todo/TodoController.java
+++ src/main/java/com/example/demo/todo/TodoController.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.todo.constant.Status;
 import com.example.demo.todo.entity.Todo;
 import com.example.demo.todo.form.TodoForm;
 import com.example.demo.todo.service.TodoService;
-import com.example.demo.todo.constant.Status;
 
 @Controller
 @RequestMapping("/")
@@ -33,7 +34,23 @@ public class TodoController {
 		model.addAttribute("TodoForm",  new TodoForm());
 		return "todo/index";
 	}
-	@RequestMapping(params="add", method=RequestMethod.POST)
+	@PostMapping("/updateTodoName")
+	public String test(Model model) {
+		List<Todo> todoList = service.getTodos();
+		model.addAttribute("todoList",todoList);
+		model.addAttribute("TodoForm",  new TodoForm());
+		return "todo/index";
+	}
+	
+	@RequestMapping(params="updateTodoName", method=RequestMethod.POST)
+	public String updateTodoName(Model model, TodoForm form, BindingResult result) {
+		List<Todo> todoList = service.getTodos();
+		model.addAttribute("todoList",todoList);
+		model.addAttribute("TodoForm",  new TodoForm());
+		return "redirect:/";
+	}
+
+	@PostMapping("/add")
 	public String add(Model model) {
 		service.addTodo();
 		List<Todo> todoList = service.getTodos();	
@@ -41,7 +58,7 @@ public class TodoController {
 		return "redirect:/";
 	}
 	
-	@RequestMapping(params="delete", method=RequestMethod.POST)
+	@PostMapping("/delete")
 	public String delete(Model model, TodoForm form, BindingResult result) {
 		List<Integer> targetIds = form.getCheckTodo();
 		for(Integer targetId: targetIds) {
@@ -51,8 +68,8 @@ public class TodoController {
 		model.addAttribute("todoList",todoList);
 		return "redirect:/";
 	}
-	
-	@RequestMapping(params="updateToIncomplete", method=RequestMethod.POST)
+
+	@PostMapping("/updateToIncomplete")
 	public String updateToInComplete(Model model, TodoForm form, BindingResult result) {
 		List<Integer> targetIds = form.getCheckTodo();
 		for(Integer targetId: targetIds) {
@@ -63,7 +80,7 @@ public class TodoController {
 		return "redirect:/";
 	}
 	
-	@RequestMapping(params="updateToRunning", method=RequestMethod.POST)
+	@PostMapping("/updateToRunning")
 	public String updateToRunning(Model model, TodoForm form, BindingResult result) {
 		List<Integer> targetIds = form.getCheckTodo();
 		for(Integer targetId: targetIds) {
@@ -74,7 +91,7 @@ public class TodoController {
 		return "redirect:/";
 	}
 	
-	@RequestMapping(params="updateToCompleted", method=RequestMethod.POST)
+	@PostMapping("/updateToCompleted")
 	public String updateToCompleted(Model model, TodoForm form, BindingResult result) {
 		List<Integer> targetIds = form.getCheckTodo();
 		for(Integer targetId: targetIds) {
